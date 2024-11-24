7e62ccf941c6ed05965c4ef1ee2fb0c2e870e079
nazmulchowdhury53 <150889716+nazmulchowdhury53@users.noreply.github.com>
2024-11-25 00:48:59 +0600

    Create 791A - Bear and Big Brother.cpp

---
+++ 791A - Bear and Big Brother.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+#include<math.h>
+#include<string.h>
+#include<vector>
+using namespace std;
+int main()
+{
+
+    long  long int a,b,c=0,d,e,i;
+    cin>>a>>b;
+    while(b>=a)
+    {
+        a*=3;
+    b*=2;
+    c++;
+
+    }
+    cout<<c<<endl;
+}
