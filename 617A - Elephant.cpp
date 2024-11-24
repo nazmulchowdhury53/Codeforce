7a6e205ae8dc1bd82fa4ff103aef7affd089e093
nazmulchowdhury53 <150889716+nazmulchowdhury53@users.noreply.github.com>
2024-11-25 00:45:07 +0600

    Create 617A - Elephant.cpp

---
+++ 617A - Elephant.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+#include<math.h>
+#include<string.h>
+#include<vector>
+using namespace std;
+int main()
+{
+
+    long long int a,b,d,e,c=0;
+    char s[51];
+    cin>>a;
+    cout<<(a+4)/5<<endl;
+}
