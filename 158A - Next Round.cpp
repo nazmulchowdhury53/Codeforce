507ce37cb5630ab99467666136bdd37474380d87
Nazmul Chowdhury <150889716+nazmulchowdhury53@users.noreply.github.com>
2024-11-30 00:28:09 +0600

    Add files via upload

---
+++ 158A - Next Round.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+#include<math.h>
+#include<string.h>
+#include<vector>
+#include<map>
+#include<algorithm>
+using namespace std;
+int main()
+{
+
+    long long int a,b,c,d,e,i,j;
+
+     cin>>a>>b>>c;
+     //d =a+b+c;
+     e= ((a+c-1)/c)*((b+c-1)/c);
+    //d =((b+c-1)/c);
+     cout<<e<<endl;
+}
