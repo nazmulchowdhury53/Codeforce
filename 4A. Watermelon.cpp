005f4801c57599dfb47749197c8c12c6b0b5dc2d
Nazmul Chowdhury <150889716+nazmulchowdhury53@users.noreply.github.com>
2024-11-30 00:20:47 +0600

    Add files via upload

---
+++ 4A. Watermelon.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+#include<math.h>
+#include<string.h>
+#include<vector>
+#include<map>
+#include<algorithm>
+using namespace std;
+int main()
+{
+
+    long long int a,b,c,d,e,i,j;
+    cin>>a;
+    if(2<a && a%2==0)
+    {
+
+        cout<<"YES"<<endl;
+    }
+    else
+        cout<<"NO"<<endl;
+}
