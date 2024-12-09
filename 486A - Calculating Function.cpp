f18eabc29259ae8a2b9f6993796a042e547c127f
Nazmul Chowdhury <150889716+nazmulchowdhury53@users.noreply.github.com>
2024-12-10 04:23:36 +0600

    Add files via upload

---
+++ 486A - Calculating Function.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+#include<math.h>
+#include<vector>
+#include<string.h>
+#include<map>
+#include<algorithm>
+
+using namespace std;
+int main()
+{
+
+
+
+   long long int a,b,c,d=0,i,j;
+   cin>>a;
+
+
+       if(a%2==0)
+   {
+
+       c=(a/2);
+   }
+   else
+   {
+       c=-(a+1)/2;
+   }
+   cout<<c<<endl;
+
+}
