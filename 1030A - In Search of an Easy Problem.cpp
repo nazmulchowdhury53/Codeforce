9faba302f8f46a0ba74bcd9e4d907e3dff14fdc0
Nazmul Chowdhury <150889716+nazmulchowdhury53@users.noreply.github.com>
2024-12-27 00:06:34 +0600

    Add files via upload

---
+++ 1030A - In Search of an Easy Problem.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+#include<math.h>
+#include<vector>
+#include<string.h>
+using namespace std;
+int main()
+{
+
+    long long int a,b,c=0,d,e,i,j,k;
+    cin>>a;
+    while(a--)
+    {
+
+        cin>>b;
+        if(b==1)
+        {
+
+            c=1;
+            break;
+        }
+    }
+    if(c==1)
+    {
+
+        cout<<"HARD"<<endl;
+    }
+    else
+        cout<<"EASY"<<endl;
+}
