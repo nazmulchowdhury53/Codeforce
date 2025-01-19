73a8807ddc7cf41b45199177960e45813ed3b09e
Nazmul Chowdhury <150889716+nazmulchowdhury53@users.noreply.github.com>
2025-01-20 03:05:12 +0600

    Add files via upload

---
+++ 451A. Game With Sticks.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+#include<math.h>
+#include<string>
+using namespace std;
+int main()
+{
+
+    long long int a,b,c,d,e,i,j;
+    cin>>a>>b;
+    if(min(a,b)%2==0)
+    {
+
+        cout<<"Malvika"<<endl;
+    }
+    else
+        cout<<"Akshat"<<endl;
+
+}
