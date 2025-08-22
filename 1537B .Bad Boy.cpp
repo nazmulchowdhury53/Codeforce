2de29b4d215ac011d1194b18f35f7b5ba9f052d6
Nazmul Chowdhury <150889716+nazmulchowdhury53@users.noreply.github.com>
2025-08-22 16:36:30 +0600

    Add files via upload

---
+++ 1537B .Bad Boy.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+#include<math.h>
+#include<string.h>
+using namespace std;
+using int64 = long long;
+int main()
+{
+ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    long long int a,b,c,d,e,i;
+    cin>>a;
+    while(a--)
+    {
+
+        cin>>b>>c>>d>>e;
+        cout<<1<<" "<<1<<" "<<b<<" "<<c<<endl;
+    }
+}
